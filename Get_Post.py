8c813bb488dc4b5978e8a01ce45047aa19613f27
lidorse <97695541+lidorse@users.noreply.github.com>
2022-02-17 18:52:32 +0200

    Add files via upload

---
+++ Get_Post.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+class Post():
+    def __init__(self,d):
+        self.__dict__ = d
+
+    def __str__(self):
+        result = ''
+        for k,v in self.__dict__.items():
+            result += f'{(k)} = {(v)}, '
+        return result
+
+def postId(ID):
+    response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{ID}')
+    response = json.loads(response.content)
+    getPostById = Post(response)
+    return getPostById
+
+print(postId(5))
+
---
+++ Shape.py
@@ -0,0 +1,31 @@
+class Shape:
+    def __init__(self,area,hekef):
+        self.__area = area
+        self.__hekef = hekef
+
+    def __str__(self):
+        return f'The class Shape returns {self.__area} and {self.__hekef}'
+
+    def __repr__(self):
+        return f'Shape{self.__area,self.__hekef}'
+
+    @property
+    def area(self):
+        return self.__area
+    @area.setter
+    def area(self,value):
+        if value > 0:
+            self.__area = value
+
+    @property
+    def hekef(self):
+        return self.__hekef
+    @hekef.setter
+    def hekef(self,value):
+        if value > 0:
+            self.__hekef = value
+
+
+sh1 = Shape(20,50)
+sh1.area = 12
+print(sh1.area)
